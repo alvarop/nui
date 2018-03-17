0ed7e80d8cf2e61c90e0cad8d49cc353bbff8c4b
alvaro <gitkraken@alvaroprieto.com>
2018-03-17 11:29:03 -0700

    Get blinky up and running with tasks

--- fw/nui/apps/blinky/src/main.c
+++ fw/nui/apps/blinky/src/main.c
@@ -21,51 +21,44 @@
 #include <string.h>
 
 #include "sysinit/sysinit.h"
+#include "console/console.h"
 #include "os/os.h"
 #include "bsp/bsp.h"
 #include "hal/hal_gpio.h"
-#ifdef ARCH_sim
-#include "mcu/mcu_sim.h"
-#endif
 
-static volatile int g_task1_loops;
+#define BLINK_TASK_PRI         (10)
+#define BLINK_STACK_SIZE       (256)
+struct os_task blink_task;
+os_stack_t blink_task_stack[BLINK_STACK_SIZE];
 
-/* For LED toggling */
-int g_led_pin;
+void blink_task_func(void *arg) {
 
-/**
- * main
- *
- * The main task for the project. This function initializes packages,
- * and then blinks the BSP LED in a loop.
- *
- * @return int NOTE: this function should never return!
- */
-int
-main(int argc, char **argv)
-{
+    hal_gpio_init_out(LED_BLINK_PIN, 1);
+
+    while (1) {
+        os_time_delay(OS_TICKS_PER_SEC);
+        hal_gpio_toggle(LED_BLINK_PIN);
+    }
+}
+
+int main(int argc, char **argv) {
     int rc;
-
-#ifdef ARCH_sim
-    mcu_sim_parse_args(argc, argv);
-#endif
 
     sysinit();
 
-    g_led_pin = LED_BLINK_PIN;
-    hal_gpio_init_out(g_led_pin, 1);
+    os_task_init(
+        &blink_task,
+        "blink_task",
+        blink_task_func,
+        NULL,
+        BLINK_TASK_PRI,
+        OS_WAIT_FOREVER,
+        blink_task_stack,
+        BLINK_STACK_SIZE);
 
     while (1) {
-        ++g_task1_loops;
-
-        /* Wait one second */
-        os_time_delay(OS_TICKS_PER_SEC);
-
-        /* Toggle the LED */
-        hal_gpio_toggle(g_led_pin);
+        os_eventq_run(os_eventq_dflt_get());
     }
     assert(0);
-
     return rc;
 }
-
